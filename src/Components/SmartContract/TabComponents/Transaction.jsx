5141f0e952ccc411ab79dd91e7405826ed708202
“Komal <kv120720@gmail.com>
2024-10-09 02:03:48 +0530

    .

--- src/Components/SmartContract/TabComponents/Transaction.jsx
+++ src/Components/SmartContract/TabComponents/Transaction.jsx
@@ -5,8 +5,8 @@ import DataTableComponent from "./DataTableComponent";
 const Transaction = () => {
     //17,34,39,0.5
     return (
-        <div className="h-lvh  px-2">
-            <div className="bg-gray-500 p-4 rounded-lg shadow w-[150%]  sm:w-full mx-auto mt-2 mb-5 pt-10 sm:min-w-96" style={{height:'95%'}} >
+        <div className="h-lvh  px-2 ">
+            <div className="bg-[rgba(55,65,81,1)] p-4 rounded-lg shadow w-[150%]  sm:w-full max-w-4xl mx-auto mt-2 mb-5 pt-10 sm:min-w-96" style={{height:'95%'}} >
             <div className="bg-[rgba(31,41,55,1)] border border-gray-200 rounded-lg w-full overflow-y-auto  h-full p-4 min-w-5/6 style-scrollbar" style={{height:'97%'}}>
             {/* p-4 */}
                 {/* Top bar for "Contract Source Code (Solidity)" */}
@@ -16,7 +16,7 @@ const Transaction = () => {
                     </span>
                     
                 </div>
-                <div className="px-1 style-scrollbar overflow-auto style-scrollbar"><DataTableComponent/></div>
+                <pre className="px-1"><DataTableComponent/></pre>
             </div>
         </div>
         </div>
